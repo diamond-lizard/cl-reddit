ba43ce6c50d99d0c7710c45fd725b2a5171eab2d
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2017-10-22 10:54:43 -0700

    Completed compatibility analysis of util.lisp

--- util.lisp
+++ util.lisp
@@ -24,13 +24,34 @@
 ;; The views and conclusions contained in the software and documentation are those
 ;; of the authors and should not be interpreted as representing official policies,
 ;; either expressed or implied, of the FreeBSD Project.
+;;----------------------------------------------------------
+;;
+;; Function existence compatiblity check result: INCOMPATIBLE
+;; Elisp has no in-package function
+;;----------------------------------------------------------
 (in-package #:cl-reddit)
 
 ;;;; Helper functions ;;;;
+;;----------------------------------------------------------
+;; Function existence compatibility check result: COMPATIBLE
+;; Elisp has: defun and make-instance.
+;;
+;; Function call syntax compatibility check result: INCOMPATIBLE
+;; eLisp has no &key keyword, and this needs to change to use
+;; cl-parsing-keywords and cl-keys
+;;----------------------------------------------------------
 (defun make-user (&key username password)
   "Make an instance of user class with username and password"
   (make-instance 'user :username username :password password))
 
+;; Function existence compatibility check result: MOSTLY COMPATIBLE
+;; eLisp has: cdr, defun, intern, let, push, and values
+;; eLisp does not have string-upcase, but upcase could be used instead
+;;
+;; Function call syntax compatibility check result: COMPATIBLE
+;; all functions are called in ways and with argumets that
+;; are completely eLisp compatible
+;;----------------------------------------------------------
 (defun format-key-args (args)
   "Format a list of key arguments"
   (let ((params))
@@ -39,6 +60,14 @@
           (push (values (intern (string-upcase `,arg) "KEYWORD")) params))
     params))
 
+;; Function existence compatibility check result: MOSTLY COMPATIBLE
+;; eLisp has: case and defun
+;; eLisp does not have string-downcase, but downcase could be used instead
+;;
+;; Function call syntax compatibility check result: COMPATIBLE
+;; all functions are called in ways and with argumets that
+;; are completely eLisp compatible
+;;----------------------------------------------------------
 (defun symbol-string(s)
   "Convert the input symbol to the correct string for api call."
   (case s
@@ -48,6 +77,37 @@
     (otherwise (string-downcase (symbol-name s)))))
 
 ;;;; Helper macros ;;;;
+;;----------------------------------------------------------
+;; Function existence compatibility check result: INCOMPATIBLE
+;; eLisp has:
+;;   defmacro,
+;;   gensym,
+;;   if,
+;;   let
+;;   let*,
+;;   loop,
+;;   make-instance
+;;   not,
+;;   progn,
+;;   setf,
+;;   string=,
+;;   when,
+;; user-modhash and user-cookie are definied in datatypes.lisp
+;;
+;; eLisp does NOT have:
+;;   drakma:cookie-jar-cookies
+;;   drakma:cookie-name
+;;   drakma:cookie-path
+;;   drakma:http-request
+;;   flexi-streams:flexi-stream-external-format
+;;   yason:parse
+;;
+;; Function call syntax compatibility check result: INCOMPATIBLE
+;; eLisp has no &body keyword, but &rest can be used instead.
+;;
+;; It is not clear why "(usr)" is used in the defmacro arg list
+;; instead of just "usr", or how compatible that is with eLisp.
+;;----------------------------------------------------------
 (defmacro with-user ((usr) &body body)
   "Does 'body' with logged-in user usr.  Logins in user if not logged-in."
   (let ((json (gensym)) (result (gensym)) (cks (gensym)))
@@ -72,6 +132,24 @@
                ,@body))))
        ,@body)))
 
+;;----------------------------------------------------------
+;; Function existence compatibility check result: COMPATIBLE
+;; eLisp has:
+;;   append,
+;;   butlast,
+;;   car,
+;;   defmacro,
+;;   if,
+;;   last,
+;;   list,
+;;   listp,
+;;   progn
+;; with-user is defined in util.lisp
+;;
+;; Function call syntax compatibility check result: COMPATIBLE
+;; all functions are called in ways and with argumets that
+;; are completely eLisp compatible
+;;----------------------------------------------------------
 (defmacro if-user-with (user then)
   `(if ,user
      (with-user (,user)
@@ -80,6 +158,22 @@
           `(,@then ,user)))
      (,@then)))
 
+;;----------------------------------------------------------
+;; Function existence compatibility check result: COMPATIBLE
+;; eLisp has:
+;;   defmcro
+;;   let
+;;   when
+;;   gensym
+;;   push
+;;   if
+;; post-request is defined in url.lisp
+;; symbol-string is definied in util.lisp
+;;
+;; Function call syntax compatibility check result: INCOMPATIBLE
+;; eLisp has no &key keyword, and this needs to change to use
+;; cl-parsing-keywords and cl-keys
+;;----------------------------------------------------------
 (defmacro api-post-generic (url user &key subreddit action id thing-id text vote spam flair-enabled sr kind title)
   "Defines generic post request"
   (let ((params (gensym)) (result (gensym)))
@@ -99,11 +193,52 @@
        (push `("uh" . ,(user-modhash ,user)) ,params)
        (post-request ,url ,user ,params))))
 
+;;----------------------------------------------------------
+;; Function existence compatibility check result: INCOMPATIBLE
+;; eLisp has:
+;;   defmacro
+;;   defun
+;;   format
+;;   intern
+;;
+;; api-post-generic is defined in util.lisp
+;; user is defined in datatypes.lisp
+;;
+;; eLisp does not have string-downcase, but downcase could be used instead
+;;
+;; Function call syntax compatibility check result: INCOMPATIBLE
+;; eLisp's format needs to be passed the "%s" format specifier
+;;  instead of "~a"
+;; *reddit* is a paramter created in globals.lisp using defparamter,
+;;  which eLisp does not have
+;;----------------------------------------------------------
 (defmacro def-post-api (api &rest args)
   "Defines an api call."
   `(defun ,(intern (format nil "API-~S" `,api)) (user ,@args)
      (api-post-generic ,(format nil "~a/api/~a.json" *reddit* (string-downcase api)) user ,@(format-key-args args))))
 
+;;----------------------------------------------------------
+;; Function existence compatibility check result: INCOMPATIBLE
+;; eLisp has:
+;;   caar
+;;   car
+;;   defmacro
+;;   let
+;;   loop
+;;   progn
+;;   push
+;;   typecase
+;;   when
+;;
+;; eLisp does NOT have string-downcase, but downcase could be used instead
+;;
+;; flet is an obsolete macro in eLisp, and cl-flet or cl-letf should
+;;   be used instead
+;;
+;; Function call syntax compatibility check result: COMPATIBLE
+;; all functions are called in ways and with argumets that
+;; are completely eLisp compatible
+;;----------------------------------------------------------
 (defmacro param-push (&rest commands)
   "Used in cl-reddit.lisp when defining get-* fns.
    Example expansion:
